af4f61af533e2bfb2aa7b4c94d1261152ed79d09
DoNgocSon <ngocsonse2001@gmail.com>
2022-07-17 15:07:07 +0700

    demo MVC

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,10 @@
 import express from "express";
+import homeController from "../controllers/homeController";
 
 let router = express.Router();
 
 let initWebRouter = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello World");
-  });
+  router.get("/", homeController.getHomePage);
   return app.use("/", router);
 };
 
